a0fb9f0ae6accdd61dac644baf67821fefb60272
nancyd333 <rgba0001@gmail.com>
2022-12-21 18:57:15 -0500

    prepping for push

---
+++ cryptoTest.js
@@ -0,0 +1,50 @@
+// //encryption is a two way process -- data is 'encrypted' using an algorithm and key
+// //you must know what the key is to decrypt or unscramble the data
+
+// //use crypto-js for encryption
+// const mySecret = 'I eat cookies for breakfast'
+
+// const secretKey = 'myPassword'
+
+// // Advanced Encryption Standard algorithm
+// const crypto = require('crypto-js')
+
+// // const myEncryption = crypto.AES.encrypt(mySecret,secretKey)
+// //in this example String(100) represents Primary Key 100
+// const myEncryption = crypto.AES.encrypt(String(100),secretKey)
+
+// console.log(myEncryption.toString()) //lets see our encrypted data
+
+// const decrypt = crypto.AES.decrypt(myEncryption.toString(),secretKey)
+// console.log(decrypt.toString(crypto.enc.Utf8))
+
+////------------------------------------------
+
+// //passwords in the database will be hashed
+// //hashing is a one way process, once data has been hashed you cannot unhash it
+// //hasing functions alwyas return a hash of equla length regardless on input
+// //hashing functions always return the same output given the same input
+// const bcrypt = require('bcrypt')
+
+// const userPassword = '12345password'
+// //whent he user signs up we want to had their password and save it in the db
+// //bcrypt refed the password back into the hash 12 times and takes extra time, this is called hash 'salt' 
+// const hashedPassword = bcrypt.hashSync(userPassword, 12)
+// console.log(hashedPassword)
+// //this returns true
+
+// //this returns false
+// //COMPARE a string to a hand (user login)
+// //this returns true
+// console.log(bcrypt.compareSync(userPassword, hashedPassword))
+// //this returns false
+// // console.log(bcrypt.compareSync('wrong', hashedPassword))
+
+
+//---------------------------
+//node js's build tin crypto pack
+//this is a sha256
+const cryptoNode = require('crypto')
+//it's predictable what the hash will be
+const hash = cryptoNode.createHash('sha256').update('a','utf8').digest()
+console.log(hash.toString('hex'))
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //required packages
 require('dotenv').config()
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const db = require('./models')
 
 //app config
 const app = express()
@@ -8,11 +10,51 @@ const PORT = process.env.PORT || 8000
 app.set('view engine', 'ejs')
 //middleware to parse request bodies (req.body) from html forms
 app.use(express.urlencoded({extended: false}))
+//tell express to parse incoming cookies
+app.use(cookieParser())
+
+//custom auth middleware that checks the cookies for a user id
+//and it finds one, look up the user in the db
+//tells all downstream routes about this user
+app.use(async (req, res, next) =>{
+    try{
+        if(req.cookies.userId){
+            //the user is logged in, lets find these in the db
+            const user = await db.user.findByPk(req.cookies.userId)
+            //mount the logged in user in the res.locals
+            res.locals.user = user
+        } else {
+            //set the logged in user to be null for conditional rendering
+            res.locals.user = null
+        }
+        //move on to the next middleware/route
+        next()
+    } catch (err){
+        console.log('error in auth middleware: ', err)
+        next() //go to the next thing
+    }
+})
+
+
+//example custom middleware (incoming request logger)
+app.use((req, res, next) => {
+    //our code goes here
+    //this is similar to 'morgan'
+    // console.log('hello from inside the middleware')
+    console.log(`incoming request: ${req.method} - ${req.url}`)
+    //res.locals are a place we can put data to share with 'downstream routes' (routes under this route)
+    // res.locals.myData = 'hello i am data'
+    //invoke next to go to the next route or middleware
+    next()
+})
 
 
 //routes and controllers
 app.get('/', (req, res) => {
-    res.render('home.ejs')
+    console.log(res.locals)
+    res.render('home.ejs', {
+        user: res.locals.user
+    })
 })
 
 //define users controllers
