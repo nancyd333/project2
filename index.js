898f42cd4db364a3c171c97c59f413b9987ae781
nancyd333 <rgba0001@gmail.com>
2023-01-02 18:13:07 -0500

    adds axios, search view and get, and adding search to favorites

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override');
+const axios = require('axios');
 const cookieParser = require('cookie-parser')
 const db = require('./models')
 const { defaults } = require('pg');
@@ -48,20 +49,100 @@ app.use(async (req, res, next) =>{
 })
 
 
-//example custom middleware (incoming request logger)
-app.use((req, res, next) => {
-    //our code goes here
-    //this is similar to 'morgan'
-    // console.log('hello from inside the middleware')
-    console.log(`incoming request: ${req.method} - ${req.url}`)
-    //res.locals are a place we can put data to share with 'downstream routes' (routes under this route)
-    // res.locals.myData = 'hello i am data'
-    //invoke next to go to the next route or middleware
-    next()
+// //example custom middleware (incoming request logger)
+// app.use((req, res, next) => {
+//     //our code goes here
+//     //this is similar to 'morgan'
+//     // console.log('hello from inside the middleware')
+//     console.log(`incoming request: ${req.method} - ${req.url}`)
+//     //res.locals are a place we can put data to share with 'downstream routes' (routes under this route)
+//     // res.locals.myData = 'hello i am data'
+//     //invoke next to go to the next route or middleware
+//     next()
+// })
+
+//functions
+
+//get data from api for one city
+async function getAqiApiData(city){
+    const aqiRequest = axios({
+      method: 'get',
+      url:  `https://api.api-ninjas.com/v1/airquality?city=${city}`,
+      headers: {'X-Api-Key': API_KEY}
+    }) 
+    
+    const aqiResponse = await aqiRequest
+    const aqiData = aqiResponse.data
+    return aqiData;
+  }
+
+  //returns the color associated with the AQI 
+  function getAqiColor(aqiIndexNum){
+    if (aqiIndexNum <= 50){
+      return 'green';
+    } else if(aqiIndexNum <= 100){
+      return 'yellow';
+    }  else if(aqiIndexNum <= 150){
+      return 'orange';
+    }  else if(aqiIndexNum <= 200){
+      return 'red';
+    }  else if(aqiIndexNum <= 300){
+      return 'purple';
+    } else if(aqiIndexNum >= 301){
+      return 'maroon';
+    } else {
+      return 'grey';
+    }
+  } 
+  
+
+  //routes and controllers
+
+app.get('/search', async (req,res)=>{
+    try{
+        let aqiData = await getAqiApiData(req.query.city) 
+        res.render('search',{ 
+        aqiData: aqiData,
+        aqiColor: getAqiColor(aqiData.overall_aqi),
+        city: req.query.city
+    })
+    } catch (err){
+        console.log("error", err)
+    }
+})
+
+app.post('/search', async(req,res)=>{
+    try{ 
+        let cityName = req.body.city
+        let titleCaseCityName = cityName.split(" ").reduce( (s, c) => s +""+(c.charAt(0).toUpperCase() + c.slice(1) +" "), '').trim();
+        let aqiData = await getAqiApiData(titleCaseCityName)  
+        const dbCity = await db.city.findOne({
+            where:{city: titleCaseCityName}
+        })
+        const newFavorite = db.favorite.findOrCreate({
+            where:{
+                cityId: dbCity.id,
+                date: new DATEONLY
+            },
+            defaults:{
+                userId: res.locals.user.id,
+                aqi: aqiData.overall_aqi,
+                aqi_co: aqiData.CO.aqi,
+                aqi_pm10: aqiData.PM10.aqi,
+                aqi_so2: aqiData.SO2.aqi,
+                aqi_pm2_5: aqiData["PM2.5"].aqi,
+                aqi_o3: aqiData.O3.aqi,
+                aqi_no2: aqiData.NO2.aqi,
+                comments: ''
+          }
+        })
+    } catch(err){
+      console.log("error",err)
+    }
 })
 
 
-//routes and controllers
+
 app.get('/', async (req, res) => {
 try{
     await res.render('map', {
